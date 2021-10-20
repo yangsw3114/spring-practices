eaa586651f074ab6f788bdff08c50574437c50e4
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-10-20 14:57:28 +0900

    aoptest

---
+++ aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
@@ -0,0 +1,54 @@
+package com.douzone.aoptest.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+	
+	@Before("execution(public com.douzone.aoptest.vo.ProductVo com.douzone.aoptest.service.ProductService.find(String))")
+	public void beforeAdvice() {
+		System.out.println("----Before Advice----");
+	}
+	
+	
+	@After("execution(* *..*.ProductService.*(..))")
+	public void afterAdvice() {
+		System.out.println("----After Advice----");
+	}
+	
+	@AfterReturning("execution(* *..*.ProductService.*(..))")
+	public void afterReturningAdvice() {
+		System.out.println("----AfterReturning Advice----");
+	}
+	
+	@AfterThrowing(value="execution(* *..*.ProductService.*(..))",throwing="ex")
+	public void afterThrowingAdvice(Throwable ex) {
+		System.out.println("----afterThrowing Advice: " + ex +"----");
+	}
+	
+	@Around("execution(* *..*.ProductService.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable{
+		//before
+		System.out.println("----Around(Before) Advice----");
+		
+		//PointCut Method실행
+		
+		//파라미터 가로채기
+		Object[] params = {"Canera"};
+		
+		Object result = pjp.proceed();
+		
+		//after
+		System.out.println("----Around(After) Advice----");
+		
+		return result;
+	}
+}
---
+++ aoptest/src/main/java/com/douzone/aoptest/main/MainApp.java
@@ -0,0 +1,22 @@
+package com.douzone.aoptest.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.douzone.aoptest.service.ProductService;
+import com.douzone.aoptest.vo.ProductVo;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = 
+				new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		
+		ProductService ps = ac.getBean(ProductService.class);
+		ProductVo vo = ps.find("TV");
+		System.out.println(vo);
+		((AbstractXmlApplicationContext)ac).close();
+	}
+
+}
